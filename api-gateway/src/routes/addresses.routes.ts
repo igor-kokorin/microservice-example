1e99bcc168f4da4d4d7f007120f37eb1635d8295
Igor Kokorin <kokorin.fear333777@yandex.ru>
2020-05-09 15:46:05 +0700

    custom validation for kladr id

--- api-gateway/src/routes/addresses.routes.ts
+++ api-gateway/src/routes/addresses.routes.ts
@@ -1,12 +1,42 @@
 import Hapi from 'hapi';
 import Joi from 'joi';
 
+const customJoi = Joi.extend((joi: any) => ({
+	base: joi.string(),
+	name: 'string',
+	language: {
+		'required': '"{{label}}" is required',
+		'length': '"{{label}}" must be of length 13',
+		'onlyNumbers': '"{{label}}" must contain only numbers'
+	},
+	rules: [
+		<any>{
+			name: 'kladrId',
+			validate(params: any, value: string, state: any, options: any) {
+				if (!value) {
+					return this.createError('string.required', { v: value }, state, options);
+				}
+
+				if (value.length !== 13) {
+					return this.createError('string.length', { v: value }, state, options);
+				}
+
+				if (!value.match(/^[0-9]+$/)) {
+					return this.createError('string.onlyNumbers', { v: value }, state, options);
+				}
+
+				return value;
+			}
+		}
+	]
+}));
+
 const routes = [
 	{
 		config: {
 			validate: {
 				params: {
-					kladrId: Joi.string().length(13).required()
+					kladrId: customJoi.string().kladrId()
 				}
 			}
 		},
